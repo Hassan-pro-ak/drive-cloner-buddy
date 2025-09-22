471588554b2393ca25fa6c87137d7c13ffb8c48d
sychox2006 <w29826371@gmail.com>
2025-09-22 08:35:43 +0500

    Update clone.js

--- server/routes/clone.js
+++ server/routes/clone.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const { google } = require("googleapis");
+const createClient = require("../config/google");
 
+// Dummy clone route
+router.post("/", async (req, res) => {
+  const { link } = req.body;
+
+  if (!link) {
+    return res.status(400).json({ error: "No link provided" });
+  }
+
+  // Normally: use Google Drive API to copy file/folder
+  return res.json({
+    message: "Cloning done (dummy)",
+    source: link,
+    status: "success"
+  });
+});
+
+module.exports = router;
